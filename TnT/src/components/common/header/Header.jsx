758ad08409e414cfd112a4b19e6c2e35e5525b8d
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-09-23 23:51:16 +0530

    bootstrap added

--- TnT/src/components/common/header/Header.jsx
+++ TnT/src/components/common/header/Header.jsx
@@ -10,23 +10,26 @@ import {
   Form,
 } from "react-bootstrap";
 
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <section className="header-section">
       <Container>
         <Row>
           <Navbar expand="lg" className="bg-body-tertiary mb-3">
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Brand href="#">
+              <NavLink to="/">WeekendsMonks</NavLink>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-lg`}
               aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
               placement="end"
             >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-                  Offcanvas
-                </Offcanvas.Title>
+              <Offcanvas.Header>
+                <h1 className="logo">Weekendsmonks</h1>
+                <span></span>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
